ee729b53fa680980ab0e98a6fbb0213e90d9285b
Beatriz Preuss <beatrizpreuss@Beatrizs-MacBook-Air.local>
2025-08-27 17:01:18 +0200

    First commit: React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <h1 className='underline text-2xl bg-red-400'>Testing Tailwind</h1>
+    </>
+  )
+}
+
+export default App
