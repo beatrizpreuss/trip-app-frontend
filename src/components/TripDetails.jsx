6a2f34ad79ca823d005db4774dc511373243866b
Beatriz Preuss <beatrizpreuss@Beatrizs-MacBook-Air.local>
2025-08-29 09:16:48 +0200

    feat: fetch data from backend

--- src/components/TripDetails.jsx
+++ src/components/TripDetails.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+
 export default function TripDetails() {
+
+    const { tripId } = useParams()
+
+    const [loading, setLoading] = useState(true)
+    const [accommodations, setaccommodations] = useState([])
+    const [foods, setFoods] = useState([])
+    const [pointsOfInterest, setPointsOfInterest] = useState([])
+
+    useEffect(() => {
+        fetch(`https://wander-wise-backend-t1wv.onrender.com/trips/${tripId}`)
+            .then(res => res.json())
+            .then(data => {
+                setaccommodations(data.accommodations)
+                setFoods(data.foods)
+                setPointsOfInterest(data.pointsOfInterest)
+                setLoading(false)
+            })
+    }, [])
+
+    function showaccommodations() {
+        return accommodations.map((accommodation, index) => (
+            <div key={index}>{accommodation.address}</div>
+        ))
+    }
+
     return (
         <>
             <h1>I'll show all the trip details here</h1>
+            <div>{loading ? "Loading..." : showaccommodations()}</div>
         </>
     )
 }
--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+
 export default function Trips() {
+
+    const [trips, setTrips] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch("https://wander-wise-backend-t1wv.onrender.com/trips")
+            .then(res => res.json())
+            .then(data => {
+                setTrips(data)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.error("Error fetching trips", err)
+                setLoading(false)
+            })
+    }, [])
+
+    function showTrips(trips) {
+        if (trips.length === 0) return "No trips found"
+        return trips.map((trip, index) => (
+            <div key={index}>{trip.name}</div>
+        ))
+    }
+
     return (
         <>
             <h1>I'll show all the trips here</h1>
+            <div>{loading ? "Loading" : showTrips(trips)}</div>
         </>
     )
 }
