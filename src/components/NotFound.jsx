3a11ab4c1989637f9e7c571ef11ec4f7b252a936
Beatriz Preuss <beatrizpreuss@Beatrizs-MacBook-Air.local>
2025-09-02 11:05:35 +0200

    NotFound: update component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom"
+
 export default function NotFound() {
     return (
-        <h2>The page you are looking for doesn't exist</h2>
+        <div className="m-25 mx-15">
+            <div className="flex flex-col justify-center items-center dark:text-zinc-100">
+                <h1 className="text-4xl font-bold">Page not found</h1>
+                <h3 className="mt-4 mb-20">The page you are looking for doesn't exist</h3>
+                <Link to="/">
+                    <button type="button" className="text-zinc-100 bg-zinc-900 hover:bg-zinc-800 hover:font-bold focus:ring-4 
+                            focus:outline-none focus:ring-zinc-300 font-medium rounded-lg text-sm px-4 py-2 text-center 
+                            dark:bg-zinc-100 dark:hover:bg-zinc-200 dark:focus:ring-zinc-800 dark:text-zinc-800">Go to Home
+                    </button>
+                </Link>
+            </div>
+        </div>
     )
 }
