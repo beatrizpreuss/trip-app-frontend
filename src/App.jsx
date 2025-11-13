3d71e2558f6fba823275d086da96cb6404b507aa
Beatriz Preuss <beatrizpreuss@Beatrizs-MacBook-Air.local>
2025-11-13 16:21:00 +0100

    fix: remove AuthProvider from App and add it to main & make AuthProvider wrap UserProvider

--- src/App.jsx
+++ src/App.jsx
@@ -20,35 +20,33 @@ import NotFound from './components/NotFound'
 function App() {
 
   return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/about" element={<About />} /> 
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
 
-          <Route element={<PrivateRoute />}>
-            <Route path="/trips" element={<TripLayout />}>
-              <Route index element={<Trips />} />
-              <Route path=":tripId" element={<TripDetails />} />
-              <Route path=":tripId/map" element={<TripMap />} />
-            </Route>
-            <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/trips" element={<TripLayout />}>
+            <Route index element={<Trips />} />
+            <Route path=":tripId" element={<TripDetails />} />
+            <Route path=":tripId/map" element={<TripMap />} />
           </Route>
-
-          <Route path="/find-destinations" element={<FormAI />} />
-          <Route path="/destination-ideas" element={<DestinationIdeas />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
-      </Routes>
-    </AuthProvider>
+
+        <Route path="/find-destinations" element={<FormAI />} />
+        <Route path="/destination-ideas" element={<DestinationIdeas />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
 const PrivateRoute = ({ children }) => {
   const { token } = useContext(AuthContext)
-  return token ? <Outlet /> : <Navigate to="/login" replace/>
+  return token ? <Outlet /> : <Navigate to="/login" replace />
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { UserProvider } from './components/UserContext.jsx'
+import { AuthProvider } from './components/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <AuthProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
